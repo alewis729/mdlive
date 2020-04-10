5f0c5dd8663e87d24f1c1807b746eae67df73010
Alfred Lewis <alewis1719@gmail.com>
2020-04-09 22:53:25 -0500

    added Editor component's simple version

--- src/components/molecules/Editor/style.js
+++ src/components/molecules/Editor/style.js
@@ -1,24 +1,14 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
-	root: {
-		// minWidth: 600,
-		// maxWidth: 720,
-		"& p": { color: theme.palette.text.tertiary },
-		"& textarea": { color: theme.palette.text.primary },
-		"& p, & textarea": {
-			// color: theme.palette.text.primary,
-			fontFamily: theme.typography.fontFamilyDisplay,
-			fontWeight: theme.typography.fontWeightRegular,
-			fontSize: 14,
-			lineHeight: "18px",
-		},
-	},
 	textArea: {
-		// backgroundColor: theme.palette.scale[4],
-		fontFamily: "inherit",
+		backgroundColor: theme.palette.scale[5],
+		padding: theme.spacing(2),
+		color: theme.palette.text.primary,
+		fontSize: 14,
+		lineHeight: "18px",
+		borderRadius: 5,
 		width: "100%",
-		// borderRadius: "inherit",
 		border: "none",
 		outline: "none",
 		resize: "none",
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,3 +1,4 @@
+export { default as Editor } from "./Editor";
 export { default as Footer } from "./Footer";
 export { default as Menu } from "./Menu";
 export { default as Navigation } from "./Navigation";
