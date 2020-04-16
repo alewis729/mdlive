9bcd1270e3578e05f6d7e42eaf6985ee50042a4a
Alfred Lewis <alewis1719@gmail.com>
2020-04-16 08:38:52 -0500

    Add ModalLeaveRoom

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -4,5 +4,6 @@ export { default as InteractionChat } from "./InteractionChat";
 export { default as InteractionSettings } from "./InteractionSettings";
 export { default as Menu } from "./Menu";
 export { default as ModalNewRoom } from "./ModalNewRoom";
+export { default as ModalLeaveRoom } from "./ModalLeaveRoom";
 export { default as Navigation } from "./Navigation";
 export { default as Viewer } from "./Viewer";
--- src/containers/InteractionsContainer.js
+++ src/containers/InteractionsContainer.js
@@ -4,16 +4,22 @@ import getConfig from "next/config";
 import io from "socket.io-client";
 import { Box } from "@material-ui/core";
 
-import { InteractionSettings, InteractionChat } from "@/components/molecules";
+import {
+	InteractionSettings,
+	InteractionChat,
+	ModalLeaveRoom,
+} from "@/components/molecules";
 import { InteractionsPanel } from "@/components/organisms";
 
 const { publicRuntimeConfig } = getConfig();
 const { SERVER_URL } = publicRuntimeConfig;
 const socket = io(SERVER_URL);
 const user = { id: "0", name: "alfred" };
+const initialModals = { leave: false, invite: false };
 
 const InteractionsContainer = ({ room, onLeave }) => {
 	const [chatMessages, setChatMessages] = useState([]);
+	const [modals, setModals] = useState(initialModals);
 
 	useEffect(() => {
 		socket.emit("user-connect", { name: user.name, room });
@@ -29,23 +35,35 @@ const InteractionsContainer = ({ room, onLeave }) => {
 		socket.emit("message", { id, name, message });
 	};
 
+	const handleLeave = () => {
+		setModals(initialModals);
+		onLeave();
+	};
+
 	return (
-		<InteractionsPanel
-			renderSettings={() => (
-				<InteractionSettings
-					people={[]}
-					renderPerson={person => <Box>{person.name}</Box>}
-					onInvite={() => console.log("invite others")}
-					onLeave={onLeave}
-				/>
-			)}
-			renderChat={() => (
-				<InteractionChat
-					chatMessages={chatMessages}
-					onMessageSubmit={handleMessageSubmit}
-				/>
-			)}
-		/>
+		<>
+			<ModalLeaveRoom
+				open={modals.leave}
+				onMainAction={handleLeave}
+				onClose={() => setModals({ ...modals, leave: false })}
+			/>
+			<InteractionsPanel
+				renderSettings={() => (
+					<InteractionSettings
+						people={[]}
+						renderPerson={person => <Box>{person.name}</Box>}
+						onInvite={() => console.log("invite others")}
+						onLeave={() => setModals({ ...modals, leave: true })}
+					/>
+				)}
+				renderChat={() => (
+					<InteractionChat
+						chatMessages={chatMessages}
+						onMessageSubmit={handleMessageSubmit}
+					/>
+				)}
+			/>
+		</>
 	);
 };
 
