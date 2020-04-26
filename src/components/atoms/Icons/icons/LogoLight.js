63566b2a15fe339da13d6b45a5d939d18284d3bb
Alfred Lewis <alewis1719@gmail.com>
2020-04-26 18:32:26 -0500

    Add light logo icon

--- src/components/atoms/Icons/icons/LogoLight.js
+++ src/components/atoms/Icons/icons/LogoLight.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { SvgIcon } from "@material-ui/core";
 
-const Logo = ({ ...props }) => (
+const LogoLight = ({ ...props }) => (
 	<SvgIcon {...props}>
 		<path
 			d="M4843.109,2353h-84.3V2101.791h84.3l62.02,84.057,62.98-84.057h83.82V2353h-84.3V2227.52l-62.5,80.666-62.02-80.666Z"
 			transform="translate(-4758.805 -2101.791)"
-			fill="#303030"
+			fill="#f2f2f2"
 		/>
 		<path
 			d="M5095.3,2352.959l97.734-135.254h-58.465V2101.939h-77.656v115.766h-58.18Z"
 			transform="translate(-4644.637 -2101.939)"
-			fill="#303030"
+			fill="#f2f2f2"
 		/>
 		<circle
 			cx="55"
@@ -23,10 +23,10 @@ const Logo = ({ ...props }) => (
 	</SvgIcon>
 );
 
-Logo.defaultProps = {
+LogoLight.defaultProps = {
 	width: "720",
 	height: "251.211",
 	viewBox: "0 0 720 251.211",
 };
 
-export default Logo;
+export default LogoLight;
--- src/components/atoms/Icons/index.js
+++ src/components/atoms/Icons/index.js
@@ -1,1 +1,2 @@
 export { default as IconLogo } from "./icons/Logo";
+export { default as IconLogoLight } from "./icons/LogoLight";
