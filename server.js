7d000707a5258264951d65fa21666883de827811
Alfred Lewis <alewis1719@gmail.com>
2020-05-19 19:12:17 -0500

    Modify server for production env, update package scripts

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const http = require("http");
 const server = http.Server(app);
 const io = require("socket.io")(server);
 const initWSConnection = require("./socket/main");
 
-const port = process.env.SERVER_PORT || 5000;
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 io.on("connection", (socket) => initWSConnection(io, socket));
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("build"));
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "build", "index.html"))
+	);
+}
 
 server.listen(port, (err) => {
 	if (err) throw err;
